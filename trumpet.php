a0f4dc4d183989d63c08bcb33e5f72fb2380251d
Dave <crazeydave@users.noreply.github.com>
2026-05-10 22:30:57 +0100

    Trumpet now uses the unity framework.

--- trumpet.php
+++ trumpet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Trumpet
  * Description: An announcement management plugin.
- * Version: 2.1.1
+ * Version: 2.2.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: The Bleeding Deacons
