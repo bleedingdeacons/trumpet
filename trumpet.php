dcd5f3896ef0529175e5d8f70aaf6272e4a683da
Dave <crazeydave@users.noreply.github.com>
2026-07-15 23:06:57 +0100

    Bump version to 2.5.2

--- trumpet.php
+++ trumpet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Trumpet
  * Description: An announcement management plugin.
- * Version: 2.5.1
+ * Version: 2.5.2
  * Requires at least: 6.0
  * Requires PHP: 8.1
  * GitHub Plugin URI: https://github.com/thebleedingdeacons/trumpet
