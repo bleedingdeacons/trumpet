ab0371f27d420b12b2ec72d9b64ddee96ca09a27
Dave <crazeydave@users.noreply.github.com>
2026-07-19 21:35:54 +0100

    Release 2.5.7

--- trumpet.php
+++ trumpet.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Trumpet
  * Description: An announcement management plugin.
- * Version: 2.5.6
+ * Version: 2.5.7
  * Requires at least: 6.0
  * Requires PHP: 8.1
  * Requires Plugins: unity
